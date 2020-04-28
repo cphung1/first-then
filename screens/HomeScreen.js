7308d3c28bc43161e23fabbcc18d892309c6f1a1
cphung1 <cphung97@gmail.com>
2020-04-27 20:41:05 -0600

    installed expo-image-picker && basic layout of first & then page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,15 +2,26 @@ import * as WebBrowser from 'expo-web-browser';
 import * as React from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
-
-import { MonoText } from '../components/StyledText';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function HomeScreen() {
+  const [selectedImage, setSelectedImage] = React.useState(null);
+  
   return (
-    <View>
-      <ScrollView>
+    <View style={styles.container} >
+      <View style={styles.contentContainer}>
+        <Text>First</Text>
+        <TouchableOpacity style={styles.button}>
+          <MaterialCommunityIcons name="image-plus" size={200} color="#b0b0b0" />
+        </TouchableOpacity>
+      </View>
 
-      </ScrollView>
+      <View style={styles.contentContainer}>
+        <Text>Then</Text>
+        <TouchableOpacity style={styles.button} >
+          <MaterialCommunityIcons name="image-plus" size={200} color="#b0b0b0"/>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -20,5 +31,22 @@ HomeScreen.navigationOptions = {
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'space-evenly',
+  },
+  button: {
+    display: 'flex',
+    backgroundColor: '#e6e6e6',
+    borderRadius: 15,
+    padding: 10,
+    width: '60%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  },
+  contentContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
 });
