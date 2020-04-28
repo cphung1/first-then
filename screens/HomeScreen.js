73bd255f1a9fb8b407d6a90276bce2fe30ceeac3
cphung1 <cphung97@gmail.com>
2020-04-27 23:13:21 -0600

    image selector functionality works

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,24 +3,76 @@ import * as React from 'react';
 import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function HomeScreen() {
-  const [selectedImage, setSelectedImage] = React.useState(null);
+  const [firstImage, setFirstImage] = React.useState(null);
+  const [secondImage, setSecondImage] = React.useState(null);
+
+  let firstImagePicker = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setFirstImage({ localUri: pickerResult.uri });
+  }
   
+
+  let secondImagePicker = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+
+    if (pickerResult.cancelled === true) {
+      return;
+    }
+
+    setSecondImage({ localUri: pickerResult.uri });
+  }
+  
+
   return (
     <View style={styles.container} >
       <View style={styles.contentContainer}>
-        <Text>First</Text>
-        <TouchableOpacity style={styles.button}>
-          <MaterialCommunityIcons name="image-plus" size={200} color="#b0b0b0" />
-        </TouchableOpacity>
+        <Text style={styles.text}>First</Text>
+        { firstImage ? 
+          <TouchableOpacity onPress={secondImagePicker} style={styles.button} >
+            <Image style={styles.image} source={{ uri: firstImage.localUri }} />
+          </TouchableOpacity>
+          :
+          <TouchableOpacity onPress={firstImagePicker} style={styles.button}>
+            <MaterialCommunityIcons style={styles.icon} name="image-plus" size={150} color="#b0b0b0" />
+          </TouchableOpacity>
+        }
       </View>
 
       <View style={styles.contentContainer}>
-        <Text>Then</Text>
-        <TouchableOpacity style={styles.button} >
-          <MaterialCommunityIcons name="image-plus" size={200} color="#b0b0b0"/>
+        <Text style={styles.text}>Then</Text>
+        { secondImage ? 
+          <TouchableOpacity onPress={secondImagePicker} style={styles.button} >
+            <Image style={styles.image} source={{ uri: secondImage.localUri }} />
+          </TouchableOpacity>
+          :
+        <TouchableOpacity onPress={secondImagePicker} style={styles.button} >
+          <MaterialCommunityIcons style={styles.icon} name="image-plus" size={150} color="#b0b0b0"/>
         </TouchableOpacity>
+        }
       </View>
     </View>
   );
@@ -32,21 +84,36 @@ HomeScreen.navigationOptions = {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    display: 'flex',
-    justifyContent: 'space-evenly',
+    flex: 0.95,
+    marginTop: 'auto',
+    marginBottom: 'auto',
   },
   button: {
-    display: 'flex',
     backgroundColor: '#e6e6e6',
     borderRadius: 15,
     padding: 10,
-    width: '60%',
+    width: '75%',
+    height: '80%',
     marginLeft: 'auto',
     marginRight: 'auto',
   },
-  contentContainer: {
+  image: {
     display: 'flex',
+    justifyContent: 'center',
+    height: '100%',
+    width: 'auto',
+  },
+  contentContainer: {
+    flex: 1,
     alignItems: 'center',
   },
+  icon: {
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 'auto',
+    marginBottom: 'auto',
+  },
+  text: {
+    fontSize: 30,
+  }
 });
